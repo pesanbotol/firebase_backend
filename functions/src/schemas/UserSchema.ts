8485e0cfa0d9b2730011e7484d74888ad8db620c
Hari Anugrah <code.anugrah@gmail.com>
2022-05-14 16:55:15 +0800

    add user profile fields

--- functions/src/schemas/UserSchema.ts
+++ functions/src/schemas/UserSchema.ts
@@ -4,9 +4,10 @@ import { UserProfile } from '../interfaces/User'
 /**
  * Schema profile user
  */
-export const UserProfileSchema = Joi.object<UserProfile>({
+export const UserProfileSchema = Joi.object({
   registeredAt: Joi.date().required(),
   username: Joi.string().min(3).max(64),
-  description: Joi.string().max(256)
+  description: Joi.string().max(256),
+  displayName: Joi.string().min(1).max(64),
 })
   .meta({ className: 'UserProfile' })
