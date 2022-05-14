eba023f6722ffc39a56256f594664441066e7dbc
Hari Anugrah <code.anugrah@gmail.com>
2022-05-14 21:18:12 +0800

    add template midya seeder

--- functions/src/seeder/profile-seeder.ts
+++ functions/src/seeder/profile-seeder.ts
@@ -1,17 +1,34 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
+import {UserProfile} from '../interfaces/User'
+import {UserProfileSchema} from '../schemas/UserSchema'
 
 /**
  * Seed profile in `firestore`
  */
 export const seedProfile = functions.https.onCall((data, ctx) => {
-  const db = admin.firestore()
+  // const db = admin.firestore()
 
-  functions.logger.info('Mulai seeding user profile')
-  void db.collection('users').doc('random uid disini').set({
-    // isi profile si user disini
-  })
+  // functions.logger.info('Mulai seeding user profile')
+  // // for (let index = 0; index < 100.length; index++) {
+  // //   const element = 100[index];
 
-  functions.logger.info('Seledai seeding user profile')
-  return true
+  // // }
+
+  // const user: UserProfile = {
+  //   registeredAt: new Date(),
+  //   description: "dsaofsirh",
+  //   displayName: "sdaks",
+  //   username: "oeiujr"
+  // }
+  // var {value, error} = UserProfileSchema.validate(user);
+  // if (error) {
+  //   // ada error
+  //   console.log(error);
+    
+  // }
+  // void db.collection('users').doc('iejhufvghi13ew').set(user)
+
+  // functions.logger.info('Seledai seeding user profile')
+  // return true
 })
