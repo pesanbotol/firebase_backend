f60bc79577544f741e380a9f53af34682e9281a3
Hari Anugrah <code.anugrah@gmail.com>
2022-05-19 00:48:23 +0800

    create drop collections endpoint

--- functions/src/typesense/collectionSchemas.ts
+++ functions/src/typesense/collectionSchemas.ts
@@ -2,14 +2,27 @@ import * as functions from 'firebase-functions'
 import {typesenseCollections} from './config'
 import {typeClient} from './typesenseClient'
 
-export const createCollections = functions.https.onRequest(async (request, response) => {
+export const createCollections = functions.https.onCall(async (data, context) => {
   for (const collectionName in typesenseCollections) {
     const schema = typesenseCollections[collectionName]
     functions.logger.info(`creating typesense collection for ${schema.name}`)
 
-    await typeClient.collections().create(schema)
-      .then(function (data) {
-        functions.logger.info('Succesfully created index', data)
-      })
+    const data = await typeClient.collections().create(schema)
+    functions.logger.info('Succesfully created index', data)
   }
+
+  return;
+})
+
+export const dropAllCollections = functions.https.onCall(async (data, context) => {
+  const existing = await typeClient.collections().retrieve();
+
+  for (const it of existing) {
+    functions.logger.info(`dropping typesense collection for ${it.name}`)
+
+    const data = await typeClient.collections(it.name).delete()
+    functions.logger.info(`Succesfully deleted index of ${data.name}`, data)
+  }
+
+  return;
 })
