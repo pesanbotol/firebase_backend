6938ba6bf74ab4f81bc95522ad71d4f3bf5375cf
Hari Anugrah <code.anugrah@gmail.com>
2022-06-12 20:34:27 +0800

    maybe new profile seeder

--- functions/src/seeder/profile-seeder.ts
+++ functions/src/seeder/profile-seeder.ts
@@ -1,14 +1,18 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
-// import {UserCreateProfile} from '../interfaces/User';
 import {faker} from '@faker-js/faker';
-// import {UserProfileSchema} from '../schemas';
+
+function getRandomFloat(min: number, max: number, decimals: number): number {
+  const str = (Math.random() * (max - min) + min).toFixed(decimals);
+
+  return parseFloat(str);
+}
 
 /**
  * Seed profile in `firestore`
  */
-export const seedProfile = functions.https.onCall((data, ctx) => {
-  // const db = admin.firestore()
+export const seedProfile = functions.https.onCall(async (data, ctx) => {
+  const db = admin.firestore()
 
   functions.logger.info('Mulai seeding user profile');
 
@@ -24,8 +28,25 @@ export const seedProfile = functions.https.onCall((data, ctx) => {
 
     // const {error, value} = UserProfileSchema.validate(user);
     // if (error == null) {
-      // db.collection('users').add(value);
-      admin.auth().createUser({email, password})
+    // db.collection('users').add(value);
+    const user = await admin.auth().createUser({email, password})
+    db.collection("users").doc(user.uid).set({
+      username: faker.internet.userName(),
+      displayName: faker.name.firstName() + " " + faker.name.lastName()
+    })
+
+    const createPostNum = Math.floor(Math.random() * 5)
+    for (let i = 0; i < createPostNum; i++) {
+      await db.collection('bottles').add({
+        kind: "text",
+        contentText: faker.lorem.words(6),
+        createdAt: faker.date.between(new Date(2022, 5, 11, 0, 0, 0), new Date(2022, 5, 13, 0, 0, 0)),
+        geo: [
+          getRandomFloat(6.0, -11.0, 6),
+          getRandomFloat(95.0, 141, 6),
+        ]
+      })
+    }
     // }
   }
 
